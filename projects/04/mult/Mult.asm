a173771b2f5885ada96dd91a511d6d5b1a7bdd73
asada <nna51602@gmail.com>
2022-04-19 10:02:57 +0900

    add files

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,12 +1,22 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
-
-// Put your code here.
+    @R2
+    M=0 // data initialize
+    @R1
+    D=M
+    @COUNT
+    M=D // set counter
+(LOOP)
+    @COUNT
+    D=M
+    @END
+    D;JEQ // if @COUNT == 0 then goto (END)
+    @COUNT
+    M=M-1 // dec counter
+    @R0
+    D=M
+    @R2
+    M=M+D // @R2 = @R0 + @R2
+    @LOOP
+    0;JMP // goto (LOOP)
+(END)
+    @END
+    0;JMP
